2c10735f4edaea0ebcb67fd143c58e7f6cee12cb
Coorch <38720272+Coorch@users.noreply.github.com>
2019-05-07 19:36:43 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
+"""
+第一个Python程序 - hello,world！
+向伟大的Dennis M.Ritchie先生致敬
+
+version：0.1
+Author：蔡金航
+"""
+
 import sys
 
 print(sys.version)
 print(sys.version_info)
+
 print("hello,world!")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
